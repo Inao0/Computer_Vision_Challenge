516c815baf42d4c0c271d25c0c77adaf63943193
Inao <thomas.iona20@gmail.com>
2020-01-07 23:47:30 +0000

    Add first version of the challenge The visual studio project has been renamed. It needs to be tried.

---
+++ Computer_Vision_Challenge/Source.cpp
@@ -0,0 +1,143 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+Mat src;
+Mat dst;
+void callibration_on_mouse(int event, int x, int y, int flags, void* ustc);
+double getDistance(CvPoint pointO, CvPoint pointA);
+
+int main(int argc, char** argv)
+{
+	double ratio = 1; //mm/pixel
+	const std::string mainWindowName = "Live Camera";
+	const std::string callibrationWindowName = "Calibration";
+
+	cv::namedWindow(mainWindowName, cv::WINDOW_AUTOSIZE);
+
+	cv::VideoCapture cap;
+	if (argc == 1)
+	{
+		cap.open(0);  //open the first camera
+	}
+	else
+	{
+		cap.open(argv[1]);
+	}
+	if (!cap.isOpened())
+	{
+		std::cerr << "Couldn't open capture." << std::endl;
+		return (-1);
+	}
+
+	cv::Mat frame, img;
+	char key;
+	bool keepGoing = true;
+	while (keepGoing)
+	{
+		cap >> frame;
+		if (frame.empty())
+		{
+			break;
+		}
+		img = frame.clone();
+		int font = CV_FONT_HERSHEY_SIMPLEX;
+		cv::putText(img, "Press q for quit", CvPoint(10, 300), font, 0.5, CvScalar(255, 255, 255));
+		cv::putText(img, "Press c for callibrating", CvPoint(10, 320), font, 0.5, CvScalar(255, 255, 255));
+
+		cv::imshow(mainWindowName, img);
+		
+		if ((key=cv::waitKey(30)) >= 0)
+		{
+			switch (key)
+			{
+			case 'q':
+				std::cout << " q has been pressed \n";
+				keepGoing = false;
+				break;
+			case 'c':
+				std::cout << " c has been pressed \n";
+				cv::namedWindow(callibrationWindowName, cv::WINDOW_AUTOSIZE);
+				frame.copyTo(dst);
+				setMouseCallback(callibrationWindowName, callibration_on_mouse, 0);
+				cv::imshow(callibrationWindowName, frame);
+				break;
+			case 'v':
+				std::cout << " v has been pressed \n";
+				if (cvGetWindowHandle("Calibration")) {
+					std::cout << cvGetWindowHandle("Calibration") << '\n';
+					std::cout << "Please enter the distance between the two selected point and press enter : \n" << std::endl;
+					std::cin >> ratio;
+					std::cout << "ratio : " << ratio << std::endl;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	cvDestroyWindow("Live Camera");
+	return (0);
+}
+
+
+void callibration_on_mouse(int event, int x, int y, int flags, void* ustc)
+{
+	static Point pre_pt;
+	static Point cur_pt;
+	char temp_1[20];
+	char temp_2[20];
+	double length;
+
+	if (event == CV_EVENT_LBUTTONDOWN)
+	{
+		dst.copyTo(src);
+		pre_pt = Point(x, y);
+		sprintf_s(temp_1, "x:%d,y:%d", x, y);
+
+		putText(src, temp_1, Point(x, y), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 255, 255));
+		circle(src, pre_pt, 0.5, cvScalar(255, 0, 0), CV_FILLED, CV_AA, 0);
+		imshow("Calibration", src);
+	}
+
+	else if (event == EVENT_MOUSEMOVE && (flags & EVENT_FLAG_LBUTTON))
+	{
+		dst.copyTo(src);
+		cur_pt = Point(x, y);
+		sprintf_s(temp_2, "x:%d,y:%d", x, y);
+
+		putText(src, temp_2, Point(x, y), FONT_HERSHEY_SIMPLEX, 0.5, cvScalar(0, 255, 255));
+		line(src, pre_pt, cur_pt, cvScalar(0, 255, 0), 1, CV_AA, 0);
+		imshow("Calibration", src);
+	}
+
+	else if (event == CV_EVENT_LBUTTONUP)
+	{
+
+		dst.copyTo(src);
+		cur_pt = Point(x, y);
+		double dist = getDistance(pre_pt, cur_pt);
+
+
+		sprintf_s(temp_2, "x:%d,y:%d", x, y);
+
+
+		putText(src, temp_2, Point(x, y), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 255, 255));
+
+		//circle(src, cur_pt, 3, cvScalar(255, 0, 0), CV_FILLED, CV_AA, 0);
+		line(src, pre_pt, cur_pt, cvScalar(0, 255, 0), 1, CV_AA, 0);
+		imshow("Calibration", src);
+	}
+	//double dist = getDistance(pre_pt, cur_pt);
+
+}
+
+double getDistance(CvPoint pointO, CvPoint pointA)
+
+{
+	double distance;
+	distance = powf((pointO.x - pointA.x), 2) + powf((pointO.y - pointA.y), 2);
+	distance = sqrtf(distance);
+
+	return distance;
+
+}
